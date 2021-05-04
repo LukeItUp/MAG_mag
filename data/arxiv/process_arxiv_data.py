feecfd89ec2ea675ec8a592a223270cc223a2551
Luka Kavčič <luka.kavcic95@gmail.com>
2021-05-04 13:00:05 +0200

    arxiv network with paper ids and titles

--- data/arxiv/process_arxiv_data.py
+++ data/arxiv/process_arxiv_data.py
@@ -1,46 +1,66 @@
+import networkx as nx
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-csv_data = 'processed/nodeidx2paperid.csv'
-csv_out = 'processed/nodeidx2paperid_processed.csv'
-
-OUT_FILE = open(csv_out, 'w')
 PAGE_LOAD_TIME = 1 # in seconds
 firefox_options = FirefoxOptions()
 firefox_options.add_argument("user-agent=fri-masters-research")
 firefox_options.add_argument("--headless")
-driver = webdriver.Firefox(options=firefox_options, executable_path='./geckodriver')
-driver.set_page_load_timeout(5)  # page load timeout
 
 
 def get_paper_title(paper_id):
-#    driver = webdriver.Firefox(options=firefox_options, executable_path='./geckodriver')
-#    driver.set_page_load_timeout(5)
+    driver = webdriver.Firefox(options=firefox_options, executable_path='./geckodriver')
+    driver.set_page_load_timeout(5)
     try:
         driver.get(f'https://academic.microsoft.com/paper/{paper_id}')
         time.sleep(PAGE_LOAD_TIME)
         title = driver.find_element_by_xpath('.//h1[@class="name"]').text
-        #driver.close()
     except Exception:
         title = ''
-    #driver.quit()
-    #driver.close()
-    print(f'{paper_id} —> {title}')
+    driver.quit()
+    print(f'online search for: {paper_id} —> {title}')
     return title
 
 
-with open(csv_data, 'r') as file:
-    header = file.readline().strip()
-    header += ',title\n'
-    OUT_FILE.write(header)  # Write modified header
+# ----- main -----
+print("Reading edge list")
+edge_list_path = 'processed/edge.csv'
+graph = nx.read_edgelist(edge_list_path)
 
+id_data = dict()
+print("Reading nodeidx2paperid.tsv")
+with open('processed/nodeidx2paperid.csv', 'r') as file:
     for line in file:
-        data_line = line.strip()
-        paper_id = data_line.split(',')[1]
-        paper_title = get_paper_title(paper_id)
-        data_line += f',{paper_title}\n'
-        OUT_FILE.write(data_line)
+        try:
+            data = line.strip().split(',')
+            id_data[data[0]] = data[1]
+        except Exception as e:
+            continue
 
-driver.quit()
-OUT_FILE.close()
+title_data = dict()
+print("Reading titleabs.tsv")
+# get tsv here: https://snap.stanford.edu/ogb/data/misc/ogbn_arxiv/titleabs.tsv.gz
+with open('/home/luke/Desktop/titleabs.tsv', 'r') as file:
+    for line in file:
+        try:
+            data = line.strip().split('\t')
+            title_data[data[0]] = data[1]
+        except Exception as e:
+            continue
+
+print("Adding title data to graph")
+n = len(graph)
+i = 0
+for node in graph:
+    print(f'{i}/{n}')
+    paper_id = id_data.pop(node)
+    try:
+        title = title_data.pop(paper_id)
+    except Exception as e:
+        title = get_paper_title(paper_id)
+    graph.nodes[node]['paper_id'] = paper_id
+    graph.nodes[node]['title'] = title
+    i += 1
+
+nx.write_pajek(graph, '../arxiv_network.net')

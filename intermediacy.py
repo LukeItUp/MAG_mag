16114b45b905726ca3507f3c1e98f62281d20d88
Luka Kavčič <luka.kavcic95@gmail.com>
2021-05-05 16:16:55 +0200

    added another p function

--- intermediacy.py
+++ intermediacy.py
@@ -25,12 +25,6 @@ def prepare_fixed_weights(graph, p=0.5):
     return weights
 
 
-def f_pow_x_alpha(weights, alpha=2):
-    for key in weights.keys():
-        weights[key] = weights[key] ** 2
-    return weights
-
-
 def intermediate_subgraph(graph, source, target):
     paths = nx.all_simple_paths(graph, source, target)
     nodes = set()
@@ -81,31 +75,75 @@ def monte_carlo(graph, probabilites, source, target, samples=100000):
     for node in graph:
         intermediacies[node] = 0
 
-    for _ in range(samples):
+    print(f'0/{samples}', end='')
+    for i in range(samples):
         sample = get_sample()
         for (k,v) in sample.items():
             if v:
                 intermediacies[k] += 1
-
+        print(f'\r{i}/{samples} samples done', end='')
+    print(f'\r{i+1}/{samples} samples done')
     for (k, v) in intermediacies.items():
         intermediacies[k] = v/samples
 
     return intermediacies
 
 
+def f_pow_x_alpha(weights, alpha=2.0):
+    print(f'p function: f(x) = x^alpha | alpha: {alpha}')
+    for key in weights.keys():
+        weights[key] = weights[key] ** alpha
+    return weights
+
+
+def f_pow_alpha_x(weights, alpha=2.0):
+    print(f'p function: f(x) = alpha^x | alpha: {alpha}')
+    for key in weights.keys():
+        weights[key] = alpha ** weights[key]
+    return weights
+
+
+def f_mo(weights, alpha=None):
+    if alpha is None:
+        print('alpha is average weight')
+        alpha = sum([i for i in weights.values()]) / len(weights)
+    print(f'p function: f(x) = x / (x + alpha) | alpha: {alpha}')
+    for key in weights.keys():
+        weights[key] = weights[key]/(weights[key] + alpha)
+    return weights
+
+
 graph_path = 'data/clanek.net'
 source = "1"
 target = "12"
+n_samples = 10000000
+alpha = 10000000
+random_weights = True
+w_max = 1
+
 graph = None
 weights = dict()
 
-
 if __name__ == '__main__':
+    print('---------------------------------------')
+    print(f'network: {graph_path}')
+    print(f'source: {source} | target: {target} | max w value: {w_max}')
+
     graph = read_file(graph_path)
     intermediate_graph = intermediate_subgraph(graph, source, target)
-    weights = prepare_fixed_weights(intermediate_graph)
-    intermediacies = monte_carlo(graph, weights, source, target)
-    print("Intermediacies")
+    if random_weights:
+        weights = prepare_random_weights(graph, max_w_val=w_max)
+    else:
+        weights = prepare_fixed_weights(graph, p=w_max)
+
+    # --------------------- Weight to probability functions ---------------------
+    #probabilities = f_pow_x_alpha(weights, alpha=alpha)
+    #probabilities = f_pow_alpha_x(weights, alpha=alpha)
+    probabilities = f_mo(weights, alpha=alpha)
+    # ---------------------------------------------------------------------------
+
+    intermediacies = monte_carlo(graph, probabilities, source, target, samples=n_samples)
+    print("\nIntermediacies")
     for i in sorted([i for i in intermediacies.items()], key=lambda x: x[1], reverse=True):
         if i[0] == source:
             print("source", i[1])
